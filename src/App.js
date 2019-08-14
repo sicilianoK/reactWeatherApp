686b25864293f7d9d1423626dfedc6375a33d549
KhramovM <maximiliankhramov@ukr.net>
2019-08-15 00:00:16 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -15,33 +15,40 @@ class App extends React.Component{
     };
 
     getWeather =  async (e) => {
-        e.preventDefault();
-        const city = e.target.elements.city.value;
-        const country = e.target.elements.country.value;
-        const api_call = await fetch
-        (`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=5321545cfa19c7c3277524ba672c0866`);
-        const data = await api_call.json();
-        if (city && country) {
-            // console.log(data);
-            this.setState({
-                temperature: data.main.temp,
-                city: data.name,
-                country: data.sys.country,
-                humidity: data.main.humidity,
-                description: data.weather[0].description,
-                error: ""
-            })
-        } else {
-            this.setState({
-                temperature: undefined,
-                city: undefined,
-                country: undefined,
-                humidity: undefined,
-                description: undefined,
-                error: "Please enter the value"
-            })
-        }
-    };
+        try {
+            e.preventDefault();
+            const city = e.target.elements.city.value;
+            const country = e.target.elements.country.value;
+            const api_call = await fetch
+            (`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=5321545cfa19c7c3277524ba672c0866`);
+            const data = await api_call.json();
+            if (city && country) {
+                // console.log(data);
+                this.setState({
+                    temperature: data.main.temp,
+                    city: data.name,
+                    country: data.sys.country,
+                    humidity: data.main.humidity,
+                    description: data.weather[0].description,
+                    error: ""
+                })
+            } else {
+                this.setState({
+                    temperature: undefined,
+                    city: undefined,
+                    country: undefined,
+                    humidity: undefined,
+                    description: undefined,
+                    error: "Please enter the value"
+                })
+            }
+
+        ;
+    }
+    catch (err) {
+        console.log('fetch failed', err);
+    }
+};
     render() {
         return(
             <div className="container">
